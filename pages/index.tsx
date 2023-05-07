303e2d798a6d2e5094378236b5301b2f3d100be0
RYOTA-F <ry.fj0304@gmail.com>
2023-05-07 23:12:31 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center text-center h-[100vh] w-full">
+      <h1 className="w-full font-bold text-3xl">Amplify Test: Index Page</h1>
+    </main>
+  )
+}
